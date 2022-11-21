df51311c72163a10e786961f51282deca525edf2
EricXwh <92484538+EricXwh@users.noreply.github.com>
2022-11-21 08:09:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ny4sng)
---
+++ src/components/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card text-white bg-primary" id="banner">
+    <div class="card-body">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col">
+          <i class="fa fa-bars" style="font-size: 18px"></i>
+          FRAMEWORKS
+        </div>
+        <div class="col">
+          <button
+            type="button"
+            class="btn btn-primary float-end"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <i class="fa-solid fa-circle-plus"></i> Add
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+#banner {
+  position: relative;
+  background-color: #1666bf;
+  top: 0;
+}
+</style>
